9550d97eff1e5ff315f9901580ffb5fbd4697b35
Chiagozie Eziagor <eziagorchy@gmail.com>
2020-10-15 16:10:00 +0100

    About Me_PHP-file

---
+++ BasicSyntaxMajorTask.php
@@ -0,0 +1,26 @@
+<?php
+    //My First Major Task
+
+    /*
+        Occupation: Student
+        Gender: Female
+        SideHustleUsername: eziagorchy 
+    */
+    //---About Me tag---
+    echo "<h1>About Me</h1><hr/>";
+
+    //---My sideHustleUsername----
+    echo "<h3> eziagorchy </h3>";
+
+    //About Me
+    echo "<p>I am Chiagozie Eziagor, 
+           an intern in the SideHustle
+           program aimed at equiping youths
+           with tech skills required for their 
+           career advancement. At my free time, 
+           i enoy cooking and hanging out. 
+           Feel free to reach out to me as 
+           i am a lovable and fun person. 
+           Thank you for your time.</p>"; 
+
+?>
